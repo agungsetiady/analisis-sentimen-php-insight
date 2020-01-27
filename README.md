ec0e1ae4dfeb097b6fb1d2a7efbf9179bbb72ae6
Agung Setiady <setiadyagung@gmail.com>
2020-01-27 22:40:36 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Tes Analisis Sentimen
+- Positif
+- Netral
+- Negatif
+
+## Deskripsi
+Klasifikasi menggunakan kamus kata-kata yang terdiri dari kategori positif, netral, dan negatif. Perhitungan kemungkinan sentimen dihitung oleh Algoritma Naive Bayes. Akurasi dapat ditingkatkan dengan memodifikasi kamus kata (source) dan algoritma.
---
+++ test.php
@@ -0,0 +1,38 @@
+<?php
+if (PHP_SAPI != 'cli') {
+	echo "<pre>";
+}
+
+$strings = array(
+	1 => 'Iwan akan menang pilkada',
+	2 => 'Indro penista agama, tidak pantas jadi gubernur',
+	3 => 'kamu sudah makan?',
+	4 => 'Yusuf janji bawa indonesia lebih baik',
+	5 => 'jalanan ke kampung utan rusak berat, tolong perbaiki dong',
+);
+
+require_once __DIR__ . '/autoload.php';
+$sentiment = new \PHPInsight\Sentiment();
+
+$i=1;
+foreach ($strings as $string) {
+
+	// calculations:
+	$scores = $sentiment->score($string);
+	$class = $sentiment->categorise($string);
+
+	// output:
+	if (in_array("pos", $scores)) {
+	    echo "Got positif";
+	}
+
+	echo "\nData: ".$i;
+	echo "\nKalimat: <b>$string</b>\n";
+	echo "Arah sentimen: <b>$class</b>, nilai: ";
+	// var_dump($scores);
+	foreach ($scores as $skor) {
+		echo $skor;
+	}
+	echo "\n";
+	$i++;
+}
